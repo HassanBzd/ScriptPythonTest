a28030d96579eff18b7c26213d816046ee103ef0
hassan bouzid <hassen.bouzid@hotmail.fr>
2022-04-01 01:09:20 +0200

    tett

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ sleep = "sleep 10"
 command = "launch -a screen coordinate"
 os.system(command)
 os.system(sleep)
-command = "input tap 100 5"
-os.system(command)
+
+command = "input tap"
+x = 0
+y = 0
+
+for x in range(100):
+	for y in range(100):
+		command = command+" "+str(x)+" "+str(y)
+		os.system(command)
+
+print("Finish")
