656fa36e599febc274b1432065d17795be1345ec
hassan bouzid <hassen.bouzid@hotmail.fr>
2022-04-01 01:17:36 +0200

    test

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ command = "input tap"
 x = 0
 y = 0
 
-for x in range(100):
-	for y in range(100):
+for x in range(250,500):
+	for y in range(90,100):
 		command = "input tap"
 		command = command+" "+str(x)+" "+str(y)
+		print(command)
 		os.system(command)
 
 print("Finish")
