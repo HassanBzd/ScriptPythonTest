95e1cc0628dcb4bd2c0a1ee526efed497ee28773
hassan bouzid <hassen.bouzid@hotmail.fr>
2022-04-01 00:51:32 +0200

    tap

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 print("HelloWorld")
-sleep = 10
+sleep = "sleep 10"
 
 
 
 command = "launch -a snapchat"
 os.system(command)
-os.system("sleep ",sleep)
-command = "input tap 100 100"
+os.system(sleep)
+command = "input tap 100 5"
 os.system(command)
